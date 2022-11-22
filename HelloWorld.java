9f0ae75043d2db083fbee858d8fb9cc6795c6f7f
angeltf <angel_atf@hotmail.com>
2022-11-21 19:33:06 -0500

    Primer commit p HOL

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package pyholamundo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Comentario
+		System.out.println("Hola Mundo");
+		
+		//Result comment
+	}
+
+}
